7697cf331ea091b30769fc3c77712103a0f91418
lei <slayer12135293@hotmail.com>
2014-11-10 17:16:27 +0100

    temp fix for update product.

--- ProductManager/ProductManager.Web/Controllers/ProductController.cs
+++ ProductManager/ProductManager.Web/Controllers/ProductController.cs
@@ -17,13 +17,15 @@ namespace ProductManager.Web.Controllers
         private readonly ICustomerIdService _customerIdService;
         private readonly IProductCreateViewModelFactory _productCreateViewModelFactory;
         private readonly IUpdateViewModelProductFacotry _updateViewModelProductFacotry;
+        private readonly IProductTypeRepository _productTypeRepository;
 
-        public ProductController(IProductRepository productRepository, ICustomerIdService customerIdService, IProductCreateViewModelFactory productCreateViewModelFactory, IUpdateViewModelProductFacotry updateViewModelProductFacotry)
+        public ProductController(IProductRepository productRepository, ICustomerIdService customerIdService, IProductCreateViewModelFactory productCreateViewModelFactory, IUpdateViewModelProductFacotry updateViewModelProductFacotry, IProductTypeRepository productTypeRepository)
         {
             _productRepository = productRepository;
             _customerIdService = customerIdService;
             _productCreateViewModelFactory = productCreateViewModelFactory;
             _updateViewModelProductFacotry = updateViewModelProductFacotry;
+            _productTypeRepository = productTypeRepository;
         }
 
         // GET: Product
@@ -47,11 +49,27 @@ namespace ProductManager.Web.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Edit(CreateProductViewModel updatedProduct)
+        public async Task<ActionResult> Edit(CreateProductViewModel createProductViewModel)
         {
-            var product = await _updateViewModelProductFacotry.CreateProduct(updatedProduct);
-            await _productRepository.Update(product);
-            return RedirectToAction("Detail", "SubCategory", new { updatedProduct.SubCategoryId });
+            var product = await _updateViewModelProductFacotry.CreateProduct(createProductViewModel);
+
+      
+            //var product = await _productRepository.GetByIdAsync(createProductViewModel.ProductId);
+            //product.ImageUrl = createProductViewModel.ImageUrl;
+            //product.IsNewProduct = createProductViewModel.IsNewProduct;
+            //product.Name = createProductViewModel.Name;
+            //product.UnitPrice = createProductViewModel.UnitPrice;
+            //product.Width = createProductViewModel.Width;
+            //product.Height = createProductViewModel.Height;
+            //product.ColoCode = createProductViewModel.ColoCode;
+            //product.ColorName = createProductViewModel.ColorName;
+            //product.ProductTypeId = createProductViewModel.ProductType;
+
+            
+            
+            
+            //await _productRepository.Update(product);
+            return RedirectToAction("Detail", "SubCategory", new { createProductViewModel.SubCategoryId });
         }
 
         public async Task<ActionResult> Delete(int subCategoryId, int productId)
--- ProductManager/ProductManager.Web/Factories/IUpdateViewModelProductFacotry.cs
+++ ProductManager/ProductManager.Web/Factories/IUpdateViewModelProductFacotry.cs
@@ -12,37 +12,28 @@ namespace ProductManager.Web.Factories
 
     public class UpdateViewModelProductFacotry : IUpdateViewModelProductFacotry
     {
-        private readonly IProductTypeRepository _productTypeRepository;
         private readonly IProductRepository _productRepository;
 
-        public UpdateViewModelProductFacotry(IProductTypeRepository productTypeRepository, IProductRepository productRepository)
+        public UpdateViewModelProductFacotry(ProductRepository productRepository)
         {
-            _productTypeRepository = productTypeRepository;
             _productRepository = productRepository;
         }
 
         public async Task<Product> CreateProduct(CreateProductViewModel createProductViewModel)
         {
           
-            var updatedProductType = await _productTypeRepository.GetByIdAsync(createProductViewModel.ProductType);
-
-            var po = await _productRepository.GetByIdAsync(createProductViewModel.ProductId);
-            var currentSubCat = po.SubCategory;
-            //product.ImageUrl = createProductViewModel.ImageUrl;
-            //product.IsNewProduct = createProductViewModel.IsNewProduct;
-            //product.Name = createProductViewModel.Name;
-            //product.UnitPrice = createProductViewModel.UnitPrice;
-            //product.Width = createProductViewModel.Width;
-            //product.Height = createProductViewModel.Height;
-            //product.ColoCode = createProductViewModel.ColoCode;
-            //product.ColorName = createProductViewModel.ColorName;
-
-            var product = AutoMapper.Mapper.Map<Product>(createProductViewModel);
+            var product = await _productRepository.GetByIdAsync(createProductViewModel.ProductId);
+            product.ImageUrl = createProductViewModel.ImageUrl;
+            product.IsNewProduct = createProductViewModel.IsNewProduct;
+            product.Name = createProductViewModel.Name;
+            product.UnitPrice = createProductViewModel.UnitPrice;
+            product.Width = createProductViewModel.Width;
+            product.Height = createProductViewModel.Height;
+            product.ColoCode = createProductViewModel.ColoCode;
+            product.ColorName = createProductViewModel.ColorName;
             product.ProductTypeId = createProductViewModel.ProductType;
-            product.ProductType = updatedProductType;
-            product.SubCategoryId = createProductViewModel.SubCategoryId;
-            product.SubCategory = currentSubCat;
 
+            await _productRepository.Update(product);
             return product;
         }
     }
