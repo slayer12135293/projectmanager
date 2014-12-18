7ca63fb24ebd718541ab9dca0407d65be1705427
Fredrik Olsson <fredrik.olsson@kuoni.com>
2014-12-18 17:59:27 +0100

    extracted class to own file

---
+++ ProductManager/ProductManager.Web/ViewModels/PricePlanDropDownViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductManager.Web.ViewModels
+{
+    public class PricePlanDropDownViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
